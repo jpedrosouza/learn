b8c06141bca29028788b13c55f825c84f09f8559
João Pedro Souza <jsouza.1650@gmail.com>
2021-07-24 03:38:59 -0300

    initial commit, create a avarage calculator

---
+++ MediaNotas/MediaNotas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MediaNotas
+{
+    class Program
+    {
+
+        // Inicia o programa
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CalculaMediaNotas(5, 5, 5));
+        }
+
+        /// <summary>
+        /// Método responsável por calcular a média das três notas.
+        /// </summary>
+        /// <param name="primeiraNota"></param>
+        /// <param name="segundaNota"></param>
+        /// <param name="terceiraNota"></param>
+        /// <returns>
+        /// Retorna a média das três notas passadas como parâmetros.
+        /// </returns>
+
+        public static double CalculaMediaNotas(int primeiraNota, int segundaNota, int terceiraNota)
+        {
+            return (primeiraNota + segundaNota + terceiraNota) / 3;
+        }
+    }
+}
