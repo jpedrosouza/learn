a41c363fa7a896632d9ad61c10fd52c2ddd4dc98
João Pedro Souza <jsouza.1650@gmail.com>
2021-07-25 16:49:28 -0300

    create LocationData project for get location data fom postal code (cep)

---
+++ c#/LocationData/LocationData/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using LocationData.LocationModel;
+
+namespace LocationData
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+        static Uri apiUrl = new Uri("http://viacep.com.br/ws/88330096/json");
+
+        static void Main(string[] args)
+        {            
+            GetLocationData(apiUrl).Wait();
+        }
+
+        /// <summary>
+        /// Get location data from API.
+        /// </summary>
+        /// <param name="uri">Uri to call.</param>
+        /// <returns>Returns the location data response.</returns>
+
+        static async Task<String> GetLocationData(Uri uri)
+        {
+            String locationString = null;
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                locationString = await response.Content.ReadAsStringAsync();
+            }
+
+            Console.WriteLine(locationString);
+
+            return locationString;
+        }
+    }
+}
